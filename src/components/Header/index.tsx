ad35c8982fcd4138d83967867389b338901f59ba
Caravieri <caravierigui14@gmail.com>
2021-05-18 16:45:52 -0300

    aula 2

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.module.scss';
+export function Header(){
+    return(
+        <header class="headerContainer">
+          <img src = "/logo.svg" alt="Podcastr" />
+
+          <p>O melhor para você ouvir, sempre!</p> 
+
+          <span>Qui, 8 Abril</span>
+        </header>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import { Header } from "../components/Header";
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Header />
   )
 }
